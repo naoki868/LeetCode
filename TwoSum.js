d63823e53f549cbce631cfd32acf3ef33c78cb59
naoki <takom2d8@icloud.com>
2024-04-20 23:49:37 +0900

    20240420_s tudy

---
+++ TwoSum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let result = 0 
+    for (let i = 0; i<nums.length; i++){
+        for (let j = i+1; j < nums.length; j++){
+            result = nums[i]+nums[j]
+            if(result === target){
+                return [i,j]
+            }
+        }
+    }
+};
+
+// ハッシュ値として辞書に計算結果を入れておき参照して結果を返す
+var twoSumRefactor = function (nums, targest) {
+    const seen = {}
+    for (let i = 0; i < nums.length; i++){
+        const complement = target - nums[i]
+        if (seen[complement] !== undefined)
+            return [seen[complement],i]
+    }
+    seen[complement] = nums[i]
+}
---
+++ isPalindrome.js
@@ -0,0 +1,4 @@
+var isPalindrome = function (x) {
+    return x < 0 ? false : x === x.toString().split('').reverse().join('')
+    // return x < 0 ? false : (x === +x.toString().split("").reverse().join(""));
+}
