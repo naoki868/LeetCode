e5e7364a993132192d486eae32ec42442288cfc8
naoki <takom2d8@icloud.com>
2024-04-21 20:09:46 +0900

    20240421

--- romaToInt.js
+++ romaToInt.js
@@ -24,30 +24,4 @@ var romanToInt = function(s) {
         }
     }
     return result
-};/**
-* @param {string} s
-* @return {number}
-*/
-
-var romanToInt = function(s) {
-   trans = {
-       "I":1,
-       "V":5,
-       "X":10,
-       "L":50,
-       "C":100,
-       "D":500,
-       "M":1000
-   }
-   const num = s.split("")
-   let result = 0
-   for (let i = 0; i<num.length; i++){
-       if (trans[num[i]] < trans[num[i+1]]){
-           result = trans[num[i+1]] - trans[num[i]] + result
-           i++
-       }else{
-           result = trans[num[i]] + result
-       }
-   }
-   return result
 };
