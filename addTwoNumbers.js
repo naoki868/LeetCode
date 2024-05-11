0592d95024b5633384829d81a57caeb6ed024a6b
naoki <takom2d8@icloud.com>
2024-05-11 23:44:29 +0900

    20240511

---
+++ addTwoNumbers.js
@@ -0,0 +1,15 @@
+var addTwoNumbers = function (l1, l2) {
+    let dummy = new ListNode(0)
+    let current = dummy
+    let carry = 0
+
+    while (l1 || l2 || carry) {
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry
+        carry = Math.floor(sum / 10)
+        current.next = new ListNode(sum % 10)
+        current = current.next
+        l1 = l1 ? l1.next : null
+        l2 = l2 ? l2.next : null
+    }
+    return dummy.next
+};
